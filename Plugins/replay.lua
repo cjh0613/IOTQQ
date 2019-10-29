7feb40af1bb01542b86f928a629608aa2bc0d523
IOTQQ <1700487478@qq.com>
2019-10-29 15:16:11 +0800

    v2.0.0 复读机+智能回复(以语音方式回复)

--- Plugins/replay.lua
+++ Plugins/replay.lua
@@ -26,6 +26,60 @@ function ReceiveGroupMsg(CurrentQQ, data)
             }
         )
         log.notice("From Lua SendMsg Ret-->%d", luaRes.Ret)
+        return 1
+    end
+
+    if data.MsgType == "TextMsg" then --机器人智能回复接口
+        response, error_message =
+            http.request(
+            "GET",
+            "https://api.ownthink.com/bot",
+            {
+                query = "appid=xiaosi&spoken=" .. url_encode(data.Content),
+                headers = {
+                    Accept = "*/*"
+                }
+            }
+        )
+        local html = response.body --返回json数据
+        if html == nil then
+            return 1
+        end
+        local jData = json.decode(html)
+        log.info("api %s", html)
+        if jData ~= nil then
+            Api.Api_SendMsg(
+                CurrentQQ,
+                {
+                    toUser = data.FromGroupId,
+                    sendToType = 2,
+                    sendMsgType = "VoiceMsg",
+                    groupid = 0,
+                    content = "",
+                    atUser = 0,
+                    voiceUrl = "https://dds.dui.ai/runtime/v1/synthesize?voiceId=qianranfa&speed=0.8&volume=100&audioType=wav&text=" ..
+                        url_encode(jData.data.info.text), --将回复的文字转成语音并听过网络Url方式发送回复语音
+                    voiceBase64Buf = "",
+                    picUrl = "",
+                    picBase64Buf = ""
+                }
+            )
+        end
     end
     return 1
+end
+function url_encode(str)
+    if (str) then
+        str = string.gsub(str, "\n", "\r\n")
+        str =
+            string.gsub(
+            str,
+            "([^%w ])",
+            function(c)
+                return string.format("%%%02X", string.byte(c))
+            end
+        )
+        str = string.gsub(str, " ", "+")
+    end
+    return str
 end
